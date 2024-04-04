c6969c9650873ecfa5f396919a5b66cbb3317607
Варя <varya.kornilova.21@inbox.ru>
2024-04-04 22:34:53 +0300

    Подключена БД, быстро сделан ЛистВью

---
+++ NyamNyam2024/DB/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyamNyam2024.DB
+{
+    internal class DBConnection
+    {
+        public static NyamNyamEntities nnEntities = new NyamNyamEntities();
+    }
+}
