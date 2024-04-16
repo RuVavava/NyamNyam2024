95d1a057424599851687048e12b9916effc7a28a
Варя <varya.kornilova.21@inbox.ru>
2024-04-16 03:19:04 +0300

    --

--- NyamNyam2024/Pages/DishesPage.xaml.cs
+++ NyamNyam2024/Pages/DishesPage.xaml.cs
@@ -29,7 +29,6 @@ namespace NyamNyam2024.Pages
             InitializeComponent();
             CheckAvailbleOfDish.IsCheck();
 
-
             dishes = new List<Dish>(DBConnection.nnEntities.Dish.ToList());
             categories = new List<Category>(DBConnection.nnEntities.Category.ToList());
             categories.Insert(0, new Category() { Name = "All catigories"});
@@ -88,6 +87,55 @@ namespace NyamNyam2024.Pages
             {
                 dishesLV.ItemsSource = dishes;
             }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            List<Dish> dishes = new List<Dish>(DBConnection.nnEntities.Dish.ToList());
+            dishes = dishes.Where(x => x.FinalPriceInDollar <= priceSlider.Value).ToList();
+            priceslvalue.Text = $"{Math.Round(priceSlider.Value, 2)} $";
+            dishesLV.ItemsSource = dishes;
+
+        }
+
+        private void SearchByMultipleParameters()
+        {
+            List<Dish> dishes = new List<Dish>(DBConnection.nnEntities.Dish.ToList());
+
+            //ПОИСК ПО НАИМЕНОВАНИЮ И ОПИСАНИЮ
+            if (SearchNameDishesTB.Text.Length > 0)
+            {
+                dishesLV.ItemsSource = DBConnection.nnEntities.Dish.Where(i => i.Name.ToLower().StartsWith(SearchNameDishesTB.Text.Trim().ToLower())
+                    || i.Description.ToLower().StartsWith(SearchNameDishesTB.Text.Trim().ToLower())).ToList();
+            }
+
+            //ПОИСК ПО КАТЕГОРИИ
+            var name = categoriesCB.SelectedItem as Category;
+            if (categoriesCB.SelectedIndex >= 0 && name != null)
+            {
+                dishesLV.ItemsSource = DBConnection.nnEntities.Dish.Where(x => x.CategoryId == name.Id).ToList();
+                if (categoriesCB.SelectedIndex == 0)
+                {
+                    dishesLV.ItemsSource = dishes;
+                }
+
+            }
+
+            //ЧЕКБОКС
+
+            if (IngredientsChB.IsChecked == true)
+            {
+                dishesLV.ItemsSource = DBConnection.nnEntities.Dish.Where(x => x.Availble == true).ToList();
+            }
+            else
+            {
+                dishesLV.ItemsSource = dishes;
+            }
+
+            //СЛАЙДЕР
+            dishes = dishes.Where(x => x.FinalPriceInDollar <= priceSlider.Value).ToList();
+            priceslvalue.Text = $"{Math.Round(priceSlider.Value, 2)} $";
+            dishesLV.ItemsSource = dishes;
         }
     }
 }
